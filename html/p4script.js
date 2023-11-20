b451dd4dab2f10fc550eb692faba0a867b35b2d3
yahayakhalifah111 <yahayakhalifah@gmail.com>
2023-11-20 13:09:28 +0200

    dg

---
+++ html/p4script.js
@@ -0,0 +1,10 @@
+
+let js = 'amazing';
+console.log(20 + 90 + 40);
+console.log("man");
+console.log(js);
+let country = 'Nigeria';
+let continent = 'Africa';
+let population = 123456789;
+console.log(country, continent, population);
+console.log(typeof country);
---
+++ js/p2.js
@@ -0,0 +1,5 @@
+//area of a circle 
+let pi = 3.14
+let r = 2
+let area = (r*r)*pi
+console.log(area)
